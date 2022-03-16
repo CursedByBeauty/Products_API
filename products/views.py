3381a3a5d057710ce31b2f9d21905faab741bee8
Justin Goncalves <jvgoncalves85@gmail.com>
2022-03-16 12:40:15 -0400

    Created New View Function for Fetting Products By Id + Added New URLs Path for product_detail Function

--- products/views.py
+++ products/views.py
@@ -3,6 +3,7 @@ from rest_framework.response import Response
 from rest_framework import status
 from .serializers import ProductSerializer
 from .models import Product
+from products import serializers
 
 
 @api_view(['GET', 'POST'])
@@ -23,3 +24,22 @@ def products_list(request):
         
         # else:
         #     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+@api_view(['GET'])
+def product_detail(request, pk):
+    try:
+        product = Product.objects.get(pk=pk)
+        serializer = ProductSerializer(product);
+        return Response(serializer.data)
+    except Product.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND);
+    
+    
+
+
+
+
+
+
+
+
