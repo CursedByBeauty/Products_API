318b27d9ab9abd2c1a1d488bc899419bc6c6146b
Justin Goncalves <jvgoncalves85@gmail.com>
2022-03-16 15:04:05 -0400

    Made a Delete Request

--- products/views.py
+++ products/views.py
@@ -1,3 +1,4 @@
+from django.shortcuts import get_object_or_404
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
@@ -23,17 +24,23 @@ def products_list(request):
         return Response(serializer.data, status=status.HTTP_201_CREATED)
         
         # else:
-        #     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        #       return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-@api_view(['GET'])
+@api_view(['GET', 'PUT', 'DELETE'])
 def product_detail(request, pk):
-    try:
-        product = Product.objects.get(pk=pk)
+    product = get_object_or_404(Product, pk=pk)
+    if request.method == 'GET':
         serializer = ProductSerializer(product);
         return Response(serializer.data)
-    except Product.DoesNotExist:
-        return Response(status=status.HTTP_404_NOT_FOUND);
-    
+    elif request.method == 'PUT': 
+        serializer = ProductSerializer(Product, data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data)
+    elif request.method == 'DELETE':
+        product.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
     
 
 
